49e93d424204286ef0aba43540996226ed29ebd7
Lomenzo <LomenzoJVM@gmail.com>
2024-07-03 18:41:57 +0800

    try EM

--- src/main/java/web/config/WebConfig.java
+++ src/main/java/web/config/WebConfig.java
@@ -7,9 +7,14 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
+//import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
@@ -22,11 +27,15 @@ import web.model.User;
 import web.service.UserService;
 import web.service.UserServiceImp;
 
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 
 @Configuration
 @PropertySource("classpath:db.properties")
+//@EnableJpaRepositories(basePackages = {"web"})
 @EnableTransactionManagement
 @EnableWebMvc
 @ComponentScan("web")
@@ -40,23 +49,48 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Autowired
     private Environment env;
-
+/*
     @Bean
-    public SpringResourceTemplateResolver templateResolver() {
-        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
-        templateResolver.setApplicationContext(applicationContext);
-        templateResolver.setPrefix("/WEB-INF/pages/");
-        templateResolver.setSuffix(".html");
-        return templateResolver;
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(){
+
+
+
+
+        HibernateJpaVendorAdapter adapter =  new HibernateJpaVendorAdapter();
+        adapter.setShowSql(true);
+        adapter.setGenerateDdl(false);
+        adapter.setDatabasePlatform("org.hibernate.dialect.H2Dialect");
+
+
+
+        LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
+        factoryBean.setDataSource(applicationContext.getBean(DataSource.class));
+//        factoryBean.setPersistenceUnitName("TDPersistenceUnit");
+        factoryBean.setJpaVendorAdapter(adapter);
+        factoryBean.setPackagesToScan("web");
+
+        Properties jpaProp = new Properties();
+//        jpaProp.put("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
+//        jpaProp.put("hibernate.query.factory_class", "org.hibernate.hql.internal.ast.ASTQueryTranslatorFactory");
+        jpaProp.put("hibernate.hbm2ddl.auto", "update");
+        jpaProp.put("hibernate.show_sql", Boolean.parseBoolean("true"));
+//        jpaProp.put("hibernate.connection.charset", "UTF-8");
+//        jpaProp.put("hibernate.connection.release_mode", "auto");
+//        jpaProp.put("javax.persistence.validation.mode", "callback");
+        factoryBean.setJpaProperties(jpaProp);
+
+//        Map<String, Object> jpaPropertyMap = new HashMap<>();
+////        jpaPropertyMap.put("javax.persistence.validation.factory", localValidatorFactoryBean());
+////        jpaPropertyMap.put("javax.persistence.validation.group.pre-persist", "com.td.model.validation.group.PrePersistGroup,javax.validation.groups.Default");
+//        factoryBean.setJpaPropertyMap(jpaPropertyMap);
+        return factoryBean;
     }
 
     @Bean
-    public SpringTemplateEngine templateEngine() {
-        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
-        templateEngine.setTemplateResolver(templateResolver());
-        templateEngine.setEnableSpringELCompiler(true);
-        return templateEngine;
+    public PlatformTransactionManager transactionManager() {
+        return new JpaTransactionManager(entityManagerFactory().getObject());
     }
+*/
 
     @Bean
     public DataSource getDataSource() {
@@ -87,6 +121,26 @@ public class WebConfig implements WebMvcConfigurer {
         HibernateTransactionManager transactionManager = new HibernateTransactionManager();
         transactionManager.setSessionFactory(getSessionFactory().getObject());
         return transactionManager;
+    }
+
+
+
+
+    @Bean
+    public SpringResourceTemplateResolver templateResolver() {
+        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setApplicationContext(applicationContext);
+        templateResolver.setPrefix("/WEB-INF/pages/");
+        templateResolver.setSuffix(".html");
+        return templateResolver;
+    }
+
+    @Bean
+    public SpringTemplateEngine templateEngine() {
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver());
+        templateEngine.setEnableSpringELCompiler(true);
+        return templateEngine;
     }
 
     @Override
--- src/main/java/web/dao/UserDaoImp.java
+++ src/main/java/web/dao/UserDaoImp.java
@@ -6,14 +6,28 @@ import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import web.model.User;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceContext;
+
 @Repository
 public class UserDaoImp implements UserDao{
     @Autowired
     private SessionFactory sessionFactory;
 
+//    @PersistenceContext/*(name = "TDPersistenceUnit")*/
+//    private EntityManager entityManager;
+
     @Override
     public void add(User user) {
         sessionFactory.getCurrentSession().save(user);
     }
+
+//    @Override
+//    public void add(User user) {
+//        entityManager.getTransaction().begin();
+//        entityManager.persist(user);
+//        entityManager.getTransaction().commit();
+//    }
 
 }
